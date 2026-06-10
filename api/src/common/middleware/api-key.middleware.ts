c73b2f333e84353d95f37cf86886593ff498ecb2
ChrolloLucilfer633 <kolonelgallih4@gmail.com>
2026-06-10 11:29:54 +0700

    feat : move api key to environment variables

--- api/src/common/middleware/api-key.middleware.ts
+++ api/src/common/middleware/api-key.middleware.ts
@@ -7,10 +7,11 @@ import {
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+
     const apiKey = req.headers['x-api-key'];
 
-    if (apiKey !== 'kolamrenang2026') {
-      throw new UnauthorizedException('API Key salah');
+    if (apiKey !== process.env.API_KEY) {
+      throw new UnauthorizedException('API Key tidak valid');
     }
 
     next();
