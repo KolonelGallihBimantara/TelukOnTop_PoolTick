c4305f974ad5938bd21216dfabaf05236649856f
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-06-10 11:49:10 +0700

    memberi akses flutter ke backend

--- user/lib/services/api_service.dart
+++ user/lib/services/api_service.dart
@@ -3,9 +3,15 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
 
+  static const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': 'kolamrenang2026',
+  };
+
   static Future<List<dynamic>> getTickets() async {
     final res = await http.get(
       Uri.parse('http://localhost:3000/tickets'),
+      headers: headers,
     );
 
     return json.decode(res.body);
@@ -14,7 +20,7 @@ class ApiService {
   static Future<bool> beliTiket(int id, String name) async {
     final res = await http.post(
       Uri.parse('http://localhost:3000/transactions'),
-      headers: {'Content-Type': 'application/json'},
+      headers: headers,
       body: jsonEncode({
         'ticketId': id,
         'name': name,
